23340d23efe2ce94cbbeea0fb2b386a691893a46
shigaevaler@gmail.com <shigaevaler@gmail.com>
2023-06-13 22:05:59 +0400

    main.py imports fix

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,7 @@
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.websockets import WebSocket
 
-from db import point_manager as ptm
-from db import poly_manager as pm
-from db import sig
 from routing.router import api_router
-# from routing.endpoints.polygons import router
-from serializers import (DataToUpdate, inside_the_polygon, points_to_geojson,
-                         poly_to_geojson)
 
 app = FastAPI()
 
